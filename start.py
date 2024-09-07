ac1f58527e64dd7c07cf05e6b3926bd09fc48810
Aryan Dutt <aryan.dutt43@gmail.com>
2024-09-07 17:11:12 +0800

    Set up project structure and create requirements.txt

---
+++ start.py
@@ -0,0 +1,21 @@
+import os
+
+os.makedirs('src', exist_ok=True)
+os.makedirs('data', exist_ok=True)
+os.makedirs('notebooks', exist_ok=True)
+os.makedirs('models', exist_ok=True)
+
+print('Folders created: src, data, notebooks, models.')
+
+requirements_content = """
+jupyter notebook
+transformers
+torch
+gradio
+opencv-python
+"""
+
+with open('requirements.txt', 'w') as f:
+    f.write(requirements_content)
+
+print('requirements.txt file created with essential dependencies.')
